3cfa4ddc19c3e2ab30a619f9f3eaf349969476ac
Кирилл Марков <kirill-markov-2020@bk.ru>
2025-05-15 16:49:02 +0300

    Создание контроллера для просмотра животных в приюте неавторизованному пользователю

---
+++ ShelterAnimalBackend/Controllers/AnimalsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ModelsLibrary;
+using ShelterAnimalBackend.Data;
+
+namespace ShelterAnimalBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        private readonly AnimalShelterDbContext _context;
+
+        public AnimalsController(AnimalShelterDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Animal>>> GetAnimals()
+        {
+            return await _context.Animal
+                .Include(a => a.TypeAnimal)
+                .Include(a => a.AnimalStatus)
+                .ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Animal>> GetAnimal(int id)
+        {
+            var animal = await _context.Animal
+                .Include(a => a.TypeAnimal)
+                .Include(a => a.AnimalStatus)
+                .FirstOrDefaultAsync(a => a.Id == id);
+
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            return animal;
+        }
+    }
+}
